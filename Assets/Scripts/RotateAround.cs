19774762341d4aa3599a1ce4667999a3943cc9ab
Nicolas Hognon <nicolas.hognon@gmail.com>
2013-11-24 00:02:31 +0100

    add atmosphere to the project

---
+++ Assets/Scripts/RotateAround.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAround : MonoBehaviour 
+{
+	public float speed = 10;
+	public Transform rotateAroundThis;
+	public bool right;
+	public bool angry;
+	public Material calmColor;
+	public Material angryColor;
+	
+	private bool alreadyAngry = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+//		GetComponent<TrailRenderer>().material = calmColor;
+		
+		if (right)
+			transform.position = new Vector3( rotateAroundThis.position.x, rotateAroundThis.position.y + 1f, rotateAroundThis.position.z);	
+		else
+			transform.position = new Vector3( rotateAroundThis.position.x, rotateAroundThis.position.y - 1f, rotateAroundThis.position.z);
+			
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.RotateAround( rotateAroundThis.position, Vector3.forward, speed * Time.deltaTime);
+		
+		if (angry && !alreadyAngry)
+		{
+			alreadyAngry = true;
+			GetComponent<TrailRenderer>().material = angryColor;
+		}
+		
+		if (!angry && alreadyAngry)
+		{
+			alreadyAngry = false;
+			GetComponent<TrailRenderer>().material = calmColor;
+		}
+		
+	}
+}
