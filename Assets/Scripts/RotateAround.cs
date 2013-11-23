b8a4b2923f3e5e0551d0d34dd9bd8f63b4c3f147
Nicolas Hognon <nicolas.hognon@gmail.com>
2013-11-24 00:27:50 +0100

    donuts & stars

--- Assets/Scripts/RotateAround.cs
+++ Assets/Scripts/RotateAround.cs
@@ -5,6 +5,7 @@ public class RotateAround : MonoBehaviour
 {
 	public float speed = 10;
 	public Transform rotateAroundThis;
+	public float distanceFromCenter = 0.2f;
 	public bool right;
 	public bool angry;
 	public Material calmColor;
@@ -18,16 +19,16 @@ public class RotateAround : MonoBehaviour
 //		GetComponent<TrailRenderer>().material = calmColor;
 		
 		if (right)
-			transform.position = new Vector3( rotateAroundThis.position.x, rotateAroundThis.position.y + 1f, rotateAroundThis.position.z);	
+			transform.position = new Vector3( rotateAroundThis.position.x, rotateAroundThis.position.y, rotateAroundThis.position.z + distanceFromCenter);	
 		else
-			transform.position = new Vector3( rotateAroundThis.position.x, rotateAroundThis.position.y - 1f, rotateAroundThis.position.z);
+			transform.position = new Vector3( rotateAroundThis.position.x, rotateAroundThis.position.y, rotateAroundThis.position.z - distanceFromCenter);
 			
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		transform.RotateAround( rotateAroundThis.position, Vector3.forward, speed * Time.deltaTime);
+		transform.RotateAround( rotateAroundThis.position, Vector3.up, speed * Time.deltaTime);
 		
 		if (angry && !alreadyAngry)
 		{
