18be0a00939362556206d3dea921ab2842ff8fdf
Nicolas Hognon <nicolas.hognon@gmail.com>
2013-11-23 22:40:47 +0100

    update intro

--- Assets/Scripts/Intro.cs
+++ Assets/Scripts/Intro.cs
@@ -6,5 +6,13 @@ public class Intro : MonoBehaviour {
 	void LaunchGame() {
 		Application.LoadLevel("Main");
 	}
-
+	
+	void Update() {
+		if (Input.GetKeyUp(KeyCode.Escape) ||
+			Input.GetKeyUp(KeyCode.Space) ||
+			Input.GetKeyUp(KeyCode.S)) {
+		Application.LoadLevel("Main");
+		}
+	}
+	
 }
